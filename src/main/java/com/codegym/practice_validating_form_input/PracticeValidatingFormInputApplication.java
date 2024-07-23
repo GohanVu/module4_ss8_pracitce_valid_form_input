e8a31cf7bef728ceb89b1afa27c10f381b068440
GohanVu <hoangvhdata@gmail.com>
2024-07-23 09:08:23 +0700

    update

---
+++ src/main/java/com/codegym/practice_validating_form_input/PracticeValidatingFormInputApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_validating_form_input;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeValidatingFormInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeValidatingFormInputApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/practice_validating_form_input/PracticeValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_validating_form_input;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeValidatingFormInputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
